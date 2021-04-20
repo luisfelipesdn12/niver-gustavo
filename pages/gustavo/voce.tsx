25f634b31fef747d25bcf66778a0bb788de80b88
Luis Felipe <luisfelipesdn12@gmail.com>
2021-04-20 12:35:36 -0300

    feat: voce

---
+++ pages/gustavo/voce.tsx
@@ -0,0 +1,21 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+import { FullImage } from '../../src/styles';
+
+const Voce: React.FC = () => {
+    const router = useRouter();
+
+    return (
+        <FullImage
+            src="/eh.png"
+            onClick={() => {
+                const text = prompt();
+                if (text === "eh" || text === "é") {
+                    router.push("/gustavo/voce/eh");
+                }
+            }}
+        ></FullImage>
+    );
+};
+
+export default Voce;
